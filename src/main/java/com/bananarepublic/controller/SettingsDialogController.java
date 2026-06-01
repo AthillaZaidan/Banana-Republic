f3a8927979b03afd109d86b3ac26c629f73c97c2
Jordhy Branenda <ethgalleryin@gmail.com>
2026-06-01 16:29:17 +0700

    fix: icon

--- src/main/java/com/bananarepublic/controller/SettingsDialogController.java
+++ src/main/java/com/bananarepublic/controller/SettingsDialogController.java
@@ -8,11 +8,11 @@ import com.bananarepublic.ui.GameSession;
 import com.bananarepublic.ui.LivingBackground;
 import com.bananarepublic.ui.Navigator;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 
@@ -25,13 +25,13 @@ public class SettingsDialogController {
     @FXML private Slider sfxSlider;
     @FXML private Label bgmValueLabel;
     @FXML private Label sfxValueLabel;
-    @FXML private Pane saveIconPane;
-    @FXML private Pane pluginIconPane;
+    @FXML private StackPane saveIconPane;
+    @FXML private StackPane pluginIconPane;
 
     @FXML
     public void initialize() {
-        saveIconPane.getChildren().setAll(GameIcons.save());
-        pluginIconPane.getChildren().setAll(GameIcons.plugin());
+        saveIconPane.getChildren().setAll(scaledIcon(GameIcons.save(), 1.6));
+        pluginIconPane.getChildren().setAll(scaledIcon(GameIcons.plugin(), 1.6));
         animatedBgToggle.setSelected(LivingBackground.isAnimationsEnabled());
 
         AudioEngine audio = AudioEngine.get();
@@ -124,6 +124,12 @@ public class SettingsDialogController {
 
     private void close() {
         Navigator.closeOverlay(root);
+    }
+
+    private static Group scaledIcon(Group icon, double scale) {
+        icon.setScaleX(scale);
+        icon.setScaleY(scale);
+        return icon;
     }
 
     private File normalizeSaveTarget(File chosen) {
